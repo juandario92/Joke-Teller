eff5e7b84e705faa1b727a13bbc15c257ea3fe08
juandario92 <juandariorojas1992@gmail.com>
2023-06-16 14:14:55 -0300

    Finalizando Proyecto

--- script.js
+++ script.js
@@ -1,3 +1,60 @@
 
+const button = document.getElementById('button');
+const audioElement = document.getElementById('audio');
+
 // VoiceRSS Javascript SDK
 const VoiceRSS={speech:function(e){this._validate(e),this._request(e)},_validate:function(e){if(!e)throw"The settings are undefined";if(!e.key)throw"The API key is undefined";if(!e.src)throw"The text is undefined";if(!e.hl)throw"The language is undefined";if(e.c&&"auto"!=e.c.toLowerCase()){var a=!1;switch(e.c.toLowerCase()){case"mp3":a=(new Audio).canPlayType("audio/mpeg").replace("no","");break;case"wav":a=(new Audio).canPlayType("audio/wav").replace("no","");break;case"aac":a=(new Audio).canPlayType("audio/aac").replace("no","");break;case"ogg":a=(new Audio).canPlayType("audio/ogg").replace("no","");break;case"caf":a=(new Audio).canPlayType("audio/x-caf").replace("no","")}if(!a)throw"The browser does not support the audio codec "+e.c}},_request:function(e){var a=this._buildRequest(e),t=this._getXHR();t.onreadystatechange=function(){if(4==t.readyState&&200==t.status){if(0==t.responseText.indexOf("ERROR"))throw t.responseText;audioElement.src=t.responseText,audioElement.play()}},t.open("POST","https://api.voicerss.org/",!0),t.setRequestHeader("Content-Type","application/x-www-form-urlencoded; charset=UTF-8"),t.send(a)},_buildRequest:function(e){var a=e.c&&"auto"!=e.c.toLowerCase()?e.c:this._detectCodec();return"key="+(e.key||"")+"&src="+(e.src||"")+"&hl="+(e.hl||"")+"&r="+(e.r||"")+"&c="+(a||"")+"&f="+(e.f||"")+"&ssml="+(e.ssml||"")+"&b64=true"},_detectCodec:function(){var e=new Audio;return e.canPlayType("audio/mpeg").replace("no","")?"mp3":e.canPlayType("audio/wav").replace("no","")?"wav":e.canPlayType("audio/aac").replace("no","")?"aac":e.canPlayType("audio/ogg").replace("no","")?"ogg":e.canPlayType("audio/x-caf").replace("no","")?"caf":""},_getXHR:function(){try{return new XMLHttpRequest}catch(e){}try{return new ActiveXObject("Msxml3.XMLHTTP")}catch(e){}try{return new ActiveXObject("Msxml2.XMLHTTP.6.0")}catch(e){}try{return new ActiveXObject("Msxml2.XMLHTTP.3.0")}catch(e){}try{return new ActiveXObject("Msxml2.XMLHTTP")}catch(e){}try{return new ActiveXObject("Microsoft.XMLHTTP")}catch(e){}throw"The browser does not support HTTP request"}};
+
+// Boton Visible/Invisible
+function toggleButton() {
+    button.disabled = !button.disabled;
+  };
+
+// Eviando chiste a VoiceRss API
+
+function tellMe(joke){
+    VoiceRSS.speech({
+        key: '45941e580c4446e1bdbb5f9e29b8d61d',
+        src: joke,
+        hl: 'en-us',
+        v: 'Linda',
+        r: 0, 
+        c: 'mp3',
+        f: '44khz_16bit_stereo',
+        ssml: false
+    });
+}
+
+// Obtener Chistes de Joke API
+async function getJokes() {
+    let joke = "";
+    const apiUrl = "https://v2.jokeapi.dev/joke/Any";
+    try {
+        // Creamos la constante response que se va a encargar de ir a la url
+        const response = await fetch(apiUrl);
+        // La constante "data", se crea esperando que "response" pueda devolvernos los datos de la url en formato .json
+        const data = await response.json();
+
+        if(data.setup){
+            // Si existe un chiste de dos partes, "joke" va a ser igual a las dos partes 
+            joke = `${data.setup}... ${data.delivery}`;
+        } else {
+            // Si no existe chiste de dos partes, y solo de una parte, "joke" va a ser igual a el chiste.
+            joke = data.joke;
+        }
+        // Texto a Voz
+        tellMe(joke);
+
+        // Deshabilitar Boton
+        toggleButton();
+    }
+        
+    catch(error){
+        // Cath errors here
+        console.log(error);
+    }
+}
+
+// Event Listeners
+button.addEventListener("click", getJokes);
+audioElement.addEventListener("ended", toggleButton);
