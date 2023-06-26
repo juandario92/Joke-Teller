840ca9f3ead5e76f5885d6e93de274538bf62eae
juandario92 <juandariorojas1992@gmail.com>
2023-06-26 15:00:37 -0300

    Proyecto Finalizado

--- script.js
+++ script.js
@@ -1,57 +1,59 @@
-
+// Obteniendo referencias a los elementos del DOM para el botón y el audio
 const button = document.getElementById('button');
 const audioElement = document.getElementById('audio');
 
-// Boton Visible/Invisible
+// Esta función alterna el estado de deshabilitado del botón
 function toggleButton() {
+    // Cambia el estado del botón a lo contrario de lo que es actualmente
     button.disabled = !button.disabled;
   };
 
-// Eviando chiste a VoiceRss API
-
+// Esta función toma un chiste como argumento y utiliza la API de VoiceRSS para convertir el texto en voz
 function tellMe(joke){
     VoiceRSS.speech({
-        key: 'Inserte su clave api aqui',
-        src: joke,
-        hl: 'en-us',
-        v: 'Linda',
-        r: 0, 
-        c: 'mp3',
-        f: '44khz_16bit_stereo',
-        ssml: false
+        key: 'Inserte su clave api aqui',  // tu clave para la API de VoiceRSS
+        src: joke,                         // el chiste que se va a convertir en voz
+        hl: 'en-us',                       // el idioma/acento en el que se dirá el chiste
+        v: 'Linda',                        // la voz que se utilizará
+        r: 0,                              // la tasa de discurso (0 es normal)
+        c: 'mp3',                          // el formato de codificación del audio
+        f: '44khz_16bit_stereo',           // la frecuencia y calidad del audio
+        ssml: false                        // indica si se utiliza o no el Lenguaje de marcado de síntesis de voz (SSML)
     });
 }
 
-// Obtener Chistes de Joke API
+// Esta función asincrónica obtiene un chiste de la Joke API
 async function getJokes() {
-    let joke = "";
-    const apiUrl = "https://v2.jokeapi.dev/joke/Any";
+    let joke = "";                         // inicializa la variable joke
+    const apiUrl = "https://v2.jokeapi.dev/joke/Any";   // URL de la API para obtener los chistes
     try {
-        // Creamos la constante response que se va a encargar de ir a la url
+        // Fetch devuelve una promesa que se resuelve a la Respuesta a la solicitud, sea esta exitosa o no
         const response = await fetch(apiUrl);
-        // La constante "data", se crea esperando que "response" pueda devolvernos los datos de la url en formato .json
+        // response.json también devuelve una promesa que se resuelve con el resultado de parsear el cuerpo de la respuesta en texto como JSON.
         const data = await response.json();
 
         if(data.setup){
-            // Si existe un chiste de dos partes, "joke" va a ser igual a las dos partes 
+            // Si el chiste tiene una parte de 'configuración' y una parte de 'entrega', ambos son concatenados
             joke = `${data.setup}... ${data.delivery}`;
         } else {
-            // Si no existe chiste de dos partes, y solo de una parte, "joke" va a ser igual a el chiste.
+            // Si el chiste es de una sola parte, la variable joke toma ese valor
             joke = data.joke;
         }
-        // Texto a Voz
+        // El chiste obtenido se pasa a la función tellMe() para convertirlo en voz
         tellMe(joke);
 
-        // Deshabilitar Boton
+        // Desactiva el botón
         toggleButton();
     }
         
     catch(error){
-        // Cath errors here
+        // Si hay algún error durante la ejecución de la solicitud a la API o el procesamiento de los datos, se registra en la consola
         console.log(error);
     }
 }
 
-// Event Listeners
+// Agregando event listeners a los botones y elementos de audio
+// Cuando se hace click en el botón, se invoca a la función getJokes()
 button.addEventListener("click", getJokes);
+// Cuando el audio termina de reproducirse, se vuelve a activar el botón
 audioElement.addEventListener("ended", toggleButton);
